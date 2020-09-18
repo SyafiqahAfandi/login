e1918dfcc2f48c648bf232d61c1448530fa5629c
Nur Syafiqah Afandi <fiqahsyafiqah628@gmail.com>
2020-09-18 08:50:02 +0800

    Kali Pertama Upload

---
+++ Admin/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+         <title>Admin page<title>
+    </head>
+    <body>
+        <table>
+            <form>
+                <tr>
+                    <td><label for="carian">Maklumat :</label></td>
+				    <td><input type="text" name="carian" required /></td>
+                </tr>
+                <tr>
+                <td ><button type="submit">Search</button></td> 
+                </tr>
+            </from>
+        </table>
+    </body>
+</html
